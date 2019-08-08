ad780de5d0d0a4f64f6d52573f483f79ed2f115f
Max Khon <fjoe@samodelkin.net>
2019-08-08 14:07:26 +0700

    recv_pdu() receives up to 1024 bytes at once from the agentx socket into a parsing buffer. Then the buffer is interpreted as a single PDU even if the buffer contained more than one PDU.
    Fix this by using payload_length field of AgentX PDU header and
    saving remaining unparsed buffer for the next recv_pdu() call.

--- pyagentx/network.py
+++ pyagentx/network.py
@@ -18,6 +18,7 @@ logger.addHandler(NullHandler())
 import socket
 import time
 import threading
+import struct
 try:
     import queue
 except ImportError:
@@ -35,6 +36,7 @@ class Network(threading.Thread):
         self._queue = update_queue
         self._oid_list = oid_list
         self._sethandlers = sethandlers
+        self._recv_buf = b''
 
         self.session_id = 0
         self.transaction_id = 0
@@ -73,11 +75,18 @@ class Network(threading.Thread):
         self.socket.send(pdu.encode())
         
     def recv_pdu(self):
-        buf = self.socket.recv(1024)
-        if not buf: return None
+        if len(self._recv_buf) < 20:
+            self._recv_buf += self.socket.recv(1024)
+            if len(self._recv_buf) < 20:
+                return None
+        payload_len = struct.unpack('!L', self._recv_buf[16:20])[0]
+        logger.debug('buffer length {}, pdu length {}'.format(len(self._recv_buf), 20+payload_len))
         pdu = PDU()
-        pdu.decode(buf)
+        pdu.decode(self._recv_buf[:20+payload_len])
+        self._recv_buf = self._recv_buf[20+payload_len:]
         if self.debug: pdu.dump()
+        if len(self._recv_buf) > 0:
+            logger.debug('remaining buffer length {}'.format(len(self._recv_buf)))
         return pdu
 
 
